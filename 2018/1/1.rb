7166fbb14e58ce05d7350f5af300e215fe19e7bb
Aaron Agarunov <19801205+agarun@users.noreply.github.com>
2018-12-12 15:52:24 -0500

    Add day 1 in Ruby

---
+++ 2018/1/1.rb
@@ -0,0 +1,19 @@
+# 1
+puts File.open('./input', 'r').map(&:to_i).reduce(:+)
+
+# 2
+current_frequency = 0
+frequencies_reached = Hash.new(0)
+
+loop do
+  File.foreach('./input') do |line|
+    num = line.to_i
+    current_frequency += num
+    frequencies_reached[current_frequency] += 1
+
+    if frequencies_reached[current_frequency] == 2
+      puts current_frequency
+      return
+    end
+  end
+end
